4610a001e3197423f9198891810605e88f6c8aed
U-PTCNET\mpanke <mpanke@MPANKE2L.ptcnet.ptc.com>
2017-04-27 22:59:25 +0530

    minor fix

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ angular.module('FBDataFetcherApp',[])
               defer.resolve(user)
             });
           }
-         });
+         },{scope:'user_managed_groups'});
         return defer.promise;
        }
       
